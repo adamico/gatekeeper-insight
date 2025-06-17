1b00f4170604d65d243f14cb52c7f0f4b408afb5
Andrea D'Amico <andrea.damico@gmail.com>
2025-06-17 18:22:38 +0200

    graphics for new concept

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,6 @@
 class_name Main
 extends Node
 
-const ENVIRONMENT_COLOR := Color8(87, 51, 47, 255)
-
-@onready var tilemap : TileMapLayer = %Tilemap
-
 
 func _ready() -> void:
-	RenderingServer.set_default_clear_color(ENVIRONMENT_COLOR)
+	pass
